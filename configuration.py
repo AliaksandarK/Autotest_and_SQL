6d894c2927c89936744c913dd7180aef059a9a03
AliaksandarK <bboy_dream@mail.ru>
2025-07-24 14:09:05 +0100

    Файл для хранения базовых настроек и маршрутов API

---
+++ configuration.py
@@ -0,0 +1,9 @@
+# Файл для хранения базовых настроек и маршрутов API
+# Базовый URL тестируемого сервиса
+BASE_URL = 'https://f00c93cb-0b13-42d3-a4ff-76cf33185baa.serverhub.praktikum-services.ru/'
+
+# Путь к эндпоинту для создания заказа
+CREATE_ORDER_PATH = '/api/v1/orders'
+
+# Путь к эндпоинту для получения заказа по номеру трека
+GET_ORDER_BY_TRACK_PATH = '/api/v1/orders/track'
